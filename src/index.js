6997024cda983130a03f948fc866e0dceb6caf2c
Yann Guidez <an12080@ens.etsmtl.ca>
2017-02-09 18:46:27 +0100

    modifs

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ import TUIOManager from 'tuiomanager/core/TUIOManager';
 
 // Import ImageWidget
 import ImageWidget from './ImageWidget/ImageWidget';
+import VideoWidget from './VideoWidget/VideoWidget';
+import AudioWidget from './AudioWidget/AudioWidget';
 
 //Import TagListWidget
 import TagListWidget from './TagListWidget/TagListWidget'
@@ -31,7 +33,7 @@ const buildApp = () => {
   const tagListWidget = new TagListWidget(0,0,1920,1080);
     $('body').append(tagListWidget.domElem);
     const imageWidget = new ImageWidget(0, 0, 250, 333, 'assets/IMG_20150304_201145.jpg');
-    $('body').append(imageWidget.domElem);
+    //$('body').append(imageWidget.domElem);
     var projects = document.getElementById("projectSelector");
   socketIOClient.emit('getProjectList');
   socketIOClient.on('returnProjectList', function (projectList) {
@@ -65,21 +67,37 @@ function displayImages(projectName) {
     console.log(projectName);
     socketIOClient.emit("getImagesFromProject", projectName);
     socketIOClient.on("returnAllImages", function (images) {
-        console.log(images);
+        console.log(images[0]);
+        var currentMediaWidget;
         for (var i = 0; i < images.length; i++) {
-            workbench.innerHTML += "<img src='"+socketURL+"Projects/" + projectName + "/" + images[i] + "'/>";
+            var top = Math.floor(Math.random() * (700 - 10 + 1)) + 10;
+            var left = Math.floor(Math.random() * (1600 - 10 + 1)) + 10;
+            var rotation = Math.floor(Math.random() * (360 + 1));
+
+            if (images[i].type.startsWith("audio")) {
+                currentMediaWidget = new AudioWidget(left,top,200,50, socketURL+"Projects/" + projectName + "/" + images[i].url, images[i].tags, rotation);
+            }
+            if (images[i].type.startsWith("image")) {
+                currentMediaWidget = new ImageWidget(left,top,images[i].width*0.1,images[i].height*0.1, socketURL+"Projects/" + projectName + "/" + images[i].url, images[i].tags, rotation);
+            }
+            if (images[i].type.startsWith("video")) {
+                console.log(images[i])
+                currentMediaWidget = new VideoWidget(left,top,640,360, socketURL+"Projects/" + projectName + "/" + images[i].url, images[i].tags, rotation);
+
+            }
+
+            $('#workbench').append(currentMediaWidget.domElem);
+
+            //workbench.innerHTML += "<img src='"+socketURL+"Projects/" + projectName + "/" + images[i] + "'/>";
         }
-        for (var i = 0; i < workbench.children.length; i++) {
+/*        for (var i = 0; i < workbench.children.length; i++) {
             var image = workbench.children[i];
             image.style.position = "absolute";
-            var top = Math.floor(Math.random() * (1000 - 10 + 1)) + 10;
-            var left = Math.floor(Math.random() * (1900 - 10 + 1)) + 10;
-            var rotation = Math.floor(Math.random() * (360 + 1));
             image.style.top = top + "px";
             image.style.left = left + "px";
             image.style.transform = "rotate(" + rotation + "deg)";
             console.log(image);
-        }
+        }*/
     });
 }
 
